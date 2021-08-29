3b818178b4ea28ecbae5b3b74f90a2bf223252eb
Yotsev <b.yotsev@gmail.com>
2021-08-29 10:21:09 +0300

    Added first two problems from the lab

---
+++ 03.SetsAndDictionariesAdvanced/SetsAndDictionariesAdvancedLab/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStudents = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> studentGrades = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                string[] studentInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string studentName = studentInfo[0];
+                decimal studentGrade = decimal.Parse(studentInfo[1]);
+
+                if (!studentGrades.ContainsKey(studentName))
+                {
+                    studentGrades.Add(studentName, new List<decimal>());
+                }
+
+                studentGrades[studentName].Add(studentGrade);
+            }
+
+            foreach (var student in studentGrades)
+            {
+                Console.WriteLine($"{student.Key} -> {string.Join(" ",student.Value.Select(x => x.ToString())):F2} (avg: {student.Value.Average():F2})");
+            }
+        }
+    }
+}
