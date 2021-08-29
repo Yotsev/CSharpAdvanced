2b83d037b37cb6d68c890ade73b1d5ea5ce8372b
Yotsev <b.yotsev@gmail.com>
2021-08-29 11:03:42 +0300

    Added sixth problem form lab

---
+++ 03.SetsAndDictionariesAdvanced/SetsAndDictionariesAdvancedLab/06.ParkingLot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            HashSet<string> cars = new HashSet<string>();
+
+            while (command != "END")
+            {
+                string[] commandArgs = command
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string direction = commandArgs[0];
+                string licenceplate = commandArgs[1];
+
+                if (direction == "IN")
+                {
+                    cars.Add(licenceplate);
+                }
+                else
+                {
+                    cars.Remove(licenceplate);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
