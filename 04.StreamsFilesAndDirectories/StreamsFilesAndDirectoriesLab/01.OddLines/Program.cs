de1fb5303c663272631b5d8e6cafcbb986d294ae
Yotsev <b.yotsev@gmail.com>
2021-08-31 12:31:39 +0300

    Added a part of the Stream problems, and two of the problems for functional programming

---
+++ 04.StreamsFilesAndDirectories/StreamsFilesAndDirectoriesLab/01.OddLines/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _01.OddLines
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("Input.txt"))
+            {
+                int lineCounter = 0;
+                string line = await reader.ReadLineAsync();
+                
+                while (line != null)
+                {
+                    if (lineCounter % 2 == 1)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    lineCounter++;
+                    line = await reader.ReadLineAsync();
+                }
+            }
+        }
+    }
+}
