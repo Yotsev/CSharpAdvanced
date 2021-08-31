559fe7f148b76f9c2c06e52c12a392e557012e05
Yotsev <b.yotsev@gmail.com>
2021-08-31 12:57:56 +0300

    Added all of functional programming problems

---
+++ 05.FunctionalProgramming/FunctionalProgrammingLab/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> isUpperCase = w => char.IsUpper(w[0]);
+
+            string[] UpperCaseWords = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(isUpperCase)
+                .ToArray();
+
+            foreach (var word in UpperCaseWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ 05.FunctionalProgramming/FunctionalProgrammingLab/05.FilterByAge/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FilterByAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<(string name, int age), int, bool> younger = (person, age) => person.age < age;
+            Func<(string name, int age), int, bool> older = (person, age) => person.age >= age;
+
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            List<(string name, int age)> people = new List<(string name, int age)>();
+
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                string[] peopleInfo = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = peopleInfo[0];
+                int age = int.Parse(peopleInfo[1]);
+
+                people.Add((name, age));
+            }
+
+            string condition = Console.ReadLine();
+            int filter = int.Parse(Console.ReadLine());
+            string[] outputFormat = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            if (condition == "younger")
+            {
+                people = people.Where(p => younger(p,filter)).ToList();
+            }
+            else if (condition == "older")
+            {
+                people = people.Where(p => older(p, filter)).ToList();
+            }
+
+            foreach (var person in people)
+            {
+                List<string> output = new List<string>();
+                if (outputFormat.Contains("name"))
+                {
+                    output.Add(person.name);
+                }
+
+                if (outputFormat.Contains("age"))
+                {
+                    output.Add(person.age.ToString());
+                }
+
+                Console.WriteLine(string.Join(" - ",output));
+            }
+        }
+    }
+}
