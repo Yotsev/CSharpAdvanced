6d790760add6f32f1cf55e1bb0bad43d7a8d9cf1
Yotsev <b.yotsev@gmail.com>
2021-09-14 08:53:52 +0300

    Added problems form generic lab

---
+++ 08.Generics/GenericsLab/BoxOfT/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public int Count { get { return data.Count; } }
+        public Box()
+        {
+            data = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            data.Add(element);
+        }
+
+        public T Remove()
+        {
+            T elementToRemove = data.LastOrDefault();
+            data.Remove(elementToRemove);
+            return elementToRemove;
+        }
+    }
+}
