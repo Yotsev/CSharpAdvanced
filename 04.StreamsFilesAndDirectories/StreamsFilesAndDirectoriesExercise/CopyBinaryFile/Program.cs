1c83b5206b970ae6eeedd875eefa701e27bcf6ed
Yotsev <b.yotsev@gmail.com>
2021-09-26 14:11:44 +0300

    new task added

--- 04.StreamsFilesAndDirectories/StreamsFilesAndDirectoriesExercise/CopyBinaryFile/Program.cs
+++ 04.StreamsFilesAndDirectories/StreamsFilesAndDirectoriesExercise/CopyBinaryFile/Program.cs
@@ -23,12 +23,6 @@ namespace CopyBinaryFile
 
                 writer.Write(buffer, 0, buffer.Length);
             }
-
-            //using FileStream reder = new FileStream("copyMe.png",FileMode.Open,FileAccess.Read);
-            //byte[] buffer = new byte[4096];
-
-            //using FileStream writer = new FileStream("newImg.png", FileMode.Create, FileAccess.Write);
-            //reder.CopyTo(writer, buffer.Length);
         }
     }
 }
